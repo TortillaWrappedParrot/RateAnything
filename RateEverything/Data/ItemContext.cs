04e58b32f5d17d81382a27f67854a427763d9caf
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-10-07 16:17:35 -0700

    Added DB context for models

---
+++ RateEverything/Data/ItemContext.cs
@@ -0,0 +1,18 @@
+﻿using RateEverything.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace RateEverything.Data
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
