5bb54ac697b48f5e67f077cd1be664a07729dfc3
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-10-07 16:07:42 -0700

    Added Item model

--- RateEverything/Models/Item.cs
+++ RateEverything/Models/Item.cs
@@ -2,5 +2,35 @@
 {
     public class Item
     {
+        /// <summary>
+        /// ID of the item
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the item
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description for the item
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Current rating from 1-5, displaye as stars
+        /// </summary>
+        public int Rating { get; set; }
+
+        /// <summary>
+        /// Dictionary of string and int, UserID to Rating Number
+        /// </summary>
+        public Dictionary<int, int> UserRating { get; set; }
+
+        /// <summary>
+        /// Match ID to comment in order to prevent multiple people with same name
+        /// without unique identifers
+        /// </summary>
+        public Dictionary<int, string> Comments { get; set; }
     }
 }
