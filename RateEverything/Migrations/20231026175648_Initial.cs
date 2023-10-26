364d4336f54d3b89d23d20e6f1b2e40af3321192
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-10-26 11:10:54 -0700

    Updated Migration Data

--- RateEverything/Migrations/20231026175648_Initial.cs
+++ RateEverything/Migrations/20231026175648_Initial.cs
@@ -30,6 +30,8 @@ namespace RateEverything.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
